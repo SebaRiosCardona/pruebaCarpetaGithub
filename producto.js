5044bf043a650c165e8d40a75e33e024b62192bc
Seba Rios Cardona <sebassrioss@gmail.com>
2022-08-20 18:47:43 -0300

    agregue mas cosas a la pagina PRODUCTO

---
+++ producto.js
@@ -0,0 +1,6 @@
+let previewContainer = document.querySelector(".products-preview");
+let previewBox = document.querySelector(".preview");
+
+document.querySelectorAll("products-container .product").forEach(product =>{
+    
+});
