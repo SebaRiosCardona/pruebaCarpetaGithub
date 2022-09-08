357988d58bc3e705e7df335e4133f85d8e5038bb
Seba Rios Cardona <sebassrioss@gmail.com>
2022-09-08 20:50:03 -0300

    entrega clase 8/9 Eventos

--- index1.js
+++ index1.js
@@ -1,19 +1,1 @@
-//1ER ENTREGA
-console.log ("hola")
-let contador = 0;
 
-function contadorCiudadCordoba(ciu) {
-    if (ciu == "cordoba") {
-        contador++;
-        alert("sos de " + ciu)
-    } 
-}
-
-let ciudad;
-for (let i = 1; i < 4; i++) {
-    ciudad = prompt("ingresa tu ciudad");
-    contadorCiudadCordoba(ciudad);
-    console.log("la ciudad ingresada es",ciudad);
-    console.log("Ingresaste Cordoba", contador, "veces");
-    console.log("vas ingrsando la ciudad ", i, "veces");
-}
